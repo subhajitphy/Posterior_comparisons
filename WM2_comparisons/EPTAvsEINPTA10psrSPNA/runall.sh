f19b4f5babdf4d4ba2280a0668015891e1218b81
Subhajit Dandapat <subhajit.dandapat@tifr.res.in>
2023-06-18 18:19:44 +0530

    fix2

--- WM2_comparisons/EPTAvsEINPTA10psrSPNA/runall.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-for f in J*/*.ipynb ; do jupyter nbconvert --to notebook --inplace --execute  "$f" &  done
-
--- WM3_comparisons/DR2vsDR2+/.ipynb_checkpoints/runall-checkpoint.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-for f in *.ipynb ; do jupyter nbconvert --to notebook --inplace --execute  "$f" &  done
